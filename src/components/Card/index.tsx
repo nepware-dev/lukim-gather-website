2937ebeba6e9e2fc8a5c78f50bda2efd09a4429a
saugatdhimal <dhimalsaugat77@gmail.com>
2022-03-08 11:26:38 +0545

    fix mobile view font

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Card: React.FC<Props> = ({img, text}) => (
   <div className='w-[272px] max-h-[288px] flex flex-col items-center gap-[34px] rounded-2xl bg-color-white px-[34px] pt-[24px] pb-[34px] shadow-[0_20px_40px_rgba(0,0,0,0.05)]'>
     <img src={img} alt='' className='w-[102px] h-[102px]' />
-    <p className='font-sans font-[700] text-[24px] text-color-blue-primary text-center leading-[32px]'>
+    <p className='font-sans font-[700] text-[20px] leading-[24px] text-color-blue-primary text-center sm:text-[24px] sm:leading-[32px]'>
       {text}
     </p>
   </div>
