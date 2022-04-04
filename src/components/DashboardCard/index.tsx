0ae944ab4fc96dbc3c9e5ac8858a7bc2979278ee
saugatdhimal <dhimalsaugat77@gmail.com>
2022-04-04 17:03:46 +0545

    Add dashboard

---
+++ src/components/DashboardCard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardCard = () => (
+  <div className='w-[570px] h-[374px] p-[16px] rounded-lg border border-[#CCDCE8]'>
+    <p className='font-inter font-[600] text-[16px] text-color-text'>
+      Card Title
+    </p>
+  </div>
+);
+
+export default DashboardCard;
