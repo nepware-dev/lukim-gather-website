cc3f323cf68f05b2cbfc58242926920750407f45
saugatdhimal <dhimalsaugat77@gmail.com>
2022-04-15 12:53:20 +0545

    fix date format

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,7 @@
+export const formatDate = (date: string) => {
+  const d = new Date(date);
+  const ye = new Intl.DateTimeFormat('en', {year: 'numeric'}).format(d);
+  const mo = new Intl.DateTimeFormat('en', {month: 'short'}).format(d);
+  const da = new Intl.DateTimeFormat('en', {day: 'numeric'}).format(d);
+  return `${mo} ${da}, ${ye}`;
+};
