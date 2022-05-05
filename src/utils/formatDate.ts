1fe23cf8d274ca940744f68cb71f69864085b4b1
saugatdhimal <dhimalsaugat77@gmail.com>
2022-05-05 17:16:24 +0545

    Add datepicker and date filter

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,4 @@
-export const formatDate = (date: string) => {
+export const formatDate = (date: string | Date | number) => {
   const d = new Date(date);
   const ye = new Intl.DateTimeFormat('en', {year: 'numeric'}).format(d);
   const mo = new Intl.DateTimeFormat('en', {month: 'short'}).format(d);
