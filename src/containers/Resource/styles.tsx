f24c07a32c0a3381fed776245abbea01c3bc3b0c
Dipendra Pathak <subaspathak62@gmail.com>
2023-03-02 17:13:29 +0545

    Fix misc resource page

--- src/containers/Resource/styles.tsx
+++ src/containers/Resource/styles.tsx
@@ -1,6 +1,6 @@
 export default {
   content: 'sm:mt-[10vh] mt-[50px] sm:pb-0 pb-[75px]',
-  backgroundTitle: 'font-sansBold text-[36px] leading-[44px] text-color-blue mb-[12px] lg:mt-[0] lg:text-center sm:text-[48px] sm:leading-[62px]',
+  backgroundTitle: 'font-interBold text-[36px] leading-[44px] text-color-blue mb-[12px] lg:mt-[0] lg:text-center sm:text-[48px] sm:leading-[62px]',
   input: 'flex mx-auto sm:mb-[50px] mb-[20px] w-full max-w-[550px] bg-gray-200 appearance-none border border-color-white rounded-3xl py-3 px-7 text-color-text-grey focus:outline-none focus:border-color-border',
   bgContentWrapper: 'mx-auto sm:px-[5vw] max-w-[1440px] flex flex-col items-center gap-[40px] lg:flex-row lg:justify-around lg:items-start lg:gap-[20px]',
   bgContent: 'flex flex-wrap w-[100%]',
