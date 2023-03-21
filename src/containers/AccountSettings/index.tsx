18f77a611b66171b24fe233180947ef7f595748c
Dipendra Pathak <subaspathak62@gmail.com>
2023-03-21 14:21:59 +0545

    feat(settings): change phone number

--- src/containers/AccountSettings/index.tsx
+++ src/containers/AccountSettings/index.tsx
@@ -1,13 +1,17 @@
 import React, {useCallback, useState} from 'react';
 import {gql, useMutation} from '@apollo/client';
-
-import useToast from '@hooks/useToast';
+import {useSelector} from 'react-redux';
 
 import DashboardHeader from '@components/DashboardHeader';
 import DashboardLayout from '@components/DashboardLayout';
 import AccountTab from '@components/AccountTab';
 import InputField from '@components/InputField';
 import Button from '@components/Button';
+import OTPInput from '@components/OtpInput';
+
+import useToast from '@hooks/useToast';
+import {rootState} from '@store/rootReducer';
+import {PHONE_NUMBER_CHANGE, PHONE_NUMBER_CHANGE_VERIFY} from '@services/queries';
 
 import classes from './styles';
 
@@ -21,10 +25,19 @@ export const CHANGE_PASSWORD = gql`
 
 const AccountSettings = () => {
   const toast = useToast();
-  const [activeTab, setActiveTab] = useState<string>('Password');
+  const {
+    auth: {
+      user: {email},
+    },
+  } = useSelector((state: rootState) => state);
+
   const [currentPassword, setCurrentPassword] = useState<string>('');
   const [newPassword, setNewPassword] = useState<string>('');
   const [error, setError] = useState<string>('');
+  const [newPhoneNumber, setNewPhoneNumber] = useState<string>('');
+  const [pin, setPin] = useState('');
+  const [showOtpForm, setShowOtpForm] = useState<boolean>(false);
+
   const [changePassword, {loading}] = useMutation(CHANGE_PASSWORD, {
     onCompleted: () => {
       toast('success', 'Password has been successfully changed!');
@@ -63,9 +76,67 @@ const AccountSettings = () => {
     [],
   );
 
-  const handleTab = useCallback((text: string) => {
-    setActiveTab(text);
+  const handlePhoneChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setNewPhoneNumber(e.target.value);
+    },
+    [],
+  );
+
+  const [changePhone, {loading: changePhoneLoading}] = useMutation(PHONE_NUMBER_CHANGE, {
+    onCompleted: () => {
+      toast('success', 'OTP has been sent to your new mobile number.');
+      setShowOtpForm(true);
+    },
+    onError: (err) => {
+      toast('error', String(err));
+    },
+  });
+
+  const handleVerifyPhone = useCallback(async () => {
+    await changePhone({
+      variables: {
+        data: {
+          newPhoneNumber,
+        },
+      },
+    });
+  }, [changePhone, newPhoneNumber]);
+
+  const handlePinChange = useCallback(
+    (otp) => {
+      setPin(otp);
+    },
+    [],
+  );
+
+  const handleTab = useCallback(() => {
+    setShowOtpForm(false);
+    setPin('');
   }, []);
+
+  const [changePhoneVerify, {loading: verifyLoading}] = useMutation(
+    PHONE_NUMBER_CHANGE_VERIFY,
+    {
+      onCompleted: () => {
+        toast('success', 'Your phone number has been successfully changed!');
+        handleTab();
+      },
+      onError: (err) => {
+        toast('error', String(err));
+      },
+    },
+  );
+
+  const handleChangePhoneVerify = useCallback(async () => {
+    await changePhoneVerify({
+      variables: {
+        data: {
+          pin: parseInt(pin, 10),
+        },
+      },
+    });
+  }, [changePhoneVerify, pin]);
 
   return (
     <DashboardLayout>
@@ -75,36 +146,86 @@ const AccountSettings = () => {
         <div className={classes.contentWrapper}>
           <div className={classes.tabsWrapper}>
             <AccountTab
-              text='Password'
-              isActive={activeTab === 'Password'}
+              text={email ? 'Password' : 'Phone Number'}
+              isActive
               onClick={handleTab}
             />
           </div>
           <div className='w-fit'>
-            <div className={classes.inputsWrapper}>
-              <InputField
-                password
-                title='Current password'
-                placeholder='Enter your current password'
-                value={currentPassword}
-                onChange={handleCurrentPassword}
-                inputClassname={classes.input}
-              />
-              <InputField
-                password
-                title='New password'
-                placeholder='Enter your new password'
-                value={newPassword}
-                onChange={handleNewPassword}
-                inputClassname={classes.input}
-              />
-              <Button
-                text='Save Changes'
-                onClick={handleChangePassword}
-                disabled={!currentPassword || !newPassword}
-                loading={!error && loading}
-              />
-            </div>
+            {email ? (
+              <div className={classes.inputsWrapper}>
+                <InputField
+                  password
+                  title='Current password'
+                  placeholder='Enter your current password'
+                  value={currentPassword}
+                  onChange={handleCurrentPassword}
+                  inputClassname={classes.input}
+                />
+                <InputField
+                  password
+                  title='New password'
+                  placeholder='Enter your new password'
+                  value={newPassword}
+                  onChange={handleNewPassword}
+                  inputClassname={classes.input}
+                />
+                <Button
+                  text='Save Changes'
+                  onClick={handleChangePassword}
+                  disabled={!currentPassword || !newPassword}
+                  loading={!error && loading}
+                />
+              </div>
+            ) : (
+              <div>
+                {showOtpForm ? (
+                  <div className={classes.inputsWrapper}>
+                    <OTPInput
+                      autoFocus
+                      isNumberInput
+                      length={6}
+                      className={classes.otpContainer}
+                      inputClassName={classes.otpInput}
+                      onChangeOTP={handlePinChange}
+                    />
+                    <Button
+                      text='Verify'
+                      onClick={handleChangePhoneVerify}
+                      disabled={pin.length < 6 || verifyLoading}
+                      loading={changePhoneLoading}
+                    />
+                    <div className={classes.textWrapper}>
+                      <p className={classes.text}>{'Didn\'t receive code?'}</p>
+                      <button
+                        type='button'
+                        disabled={verifyLoading}
+                        className={classes.cursor}
+                        onClick={handleVerifyPhone}
+                      >
+                        <p className={classes.sendAgain}>Send it again</p>
+                      </button>
+                    </div>
+                  </div>
+                ) : (
+                  <div className={classes.inputsWrapper}>
+                    <InputField
+                      title='New phone number (with country code)'
+                      value={newPhoneNumber}
+                      placeholder='Enter your new phone number'
+                      onChange={handlePhoneChange}
+                      inputClassname={classes.input}
+                    />
+                    <Button
+                      text='Continue'
+                      onClick={handleVerifyPhone}
+                      disabled={changePhoneLoading}
+                      loading={changePhoneLoading}
+                    />
+                  </div>
+                )}
+              </div>
+            )}
           </div>
         </div>
       </div>
--- src/containers/AccountSettings/styles.ts
+++ src/containers/AccountSettings/styles.ts
@@ -5,4 +5,10 @@ export default {
   tabsWrapper: 'flex md:flex-col gap-[20px] border-r border-[#E7E8EA]',
   input: 'w-[calc(100vw-40px)] md:w-[350px] lg:w-[409px] max-w-[409px]',
   inputsWrapper: 'flex flex-col gap-[24px]',
+  otpContainer: 'flex justify-between gap-1 sm:gap-2 w-[calc(100vw-40px)] md:w-[350px] lg:w-[409px] max-w-[409px]',
+  otpInput: 'w-1/6 py-2 sm:py-3 border border-[#CCDCE8] rounded-lg font-interMedium text-[18px] sm:text-[24px] text-center',
+  textWrapper: 'flex gap-[8px]',
+  text: 'font-inter text-base text-[#585D69]',
+  sendAgain: 'font-interSemibold text-base text-[#00518B]',
+  cursor: 'cursor-pointer',
 };
